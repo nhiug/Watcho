1aceab2f02226f22c1728c80af4603a366a67748
Deepanshu Garg <50482621+nhiug@users.noreply.github.com>
2024-05-16 12:28:03 +0530

    Update contants.js

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyBC_0fCsZ-OOoBnQKfJBgYwZxaxXaOFtKE";
+
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=80&regionCode=IN&key=" +
